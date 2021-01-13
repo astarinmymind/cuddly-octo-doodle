0e947fca6fca382fb06bef8de0faeeef9467180a
AngelaLu <angelfish@g.ucla.edu>
2021-01-12 16:36:21 -0800

    advanced types

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -54,7 +54,7 @@ interface AddFn {
     (a: number, b: number): number;
 }
 
-let add: AddFn;
-add = (n1: number, n2: number) => {
+let add7: AddFn;
+add7 = (n1: number, n2: number) => {
     return n1 + n2;
 }
